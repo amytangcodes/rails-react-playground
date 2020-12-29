c1bbd19bfa168affe74849b4a307aa74057b71ed
Amy <amytang77@gmail.com>
2020-12-29 09:13:55 -0500

    Generate a new React component

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class HelloWorld extends Component {
+  render() {
+    return <React.Fragment>Greeting: {this.props.greeting}</React.Fragment>;
+  }
+}
+
+HelloWorld.propTypes = {
+  greeting: PropTypes.string,
+};
+export default HelloWorld;
